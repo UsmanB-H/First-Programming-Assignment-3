48ab4a80f9c98de8884eb6984e10cb80e7dc9a2a
Usman Siraj Benrbia-Harni <usmanbenrbia@gmail.com>
2025-01-28 20:43:39 -0500

    firebase changes

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,26 +1,27 @@
-import { initializeApp } from 'firebase/app';
-import { getAuth, initializeAuth, getReactNativePersistence } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
-import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
+import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  initializeAuth,
+  getReactNativePersistence,
+} from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import ReactNativeAsyncStorage from "@react-native-async-storage/async-storage";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAJkuiQEgCgFLd2Uts4b8H6Pwx6izHChkE",
-    authDomain: "fpaexpo.firebaseapp.com",
-    projectId: "fpaexpo",
-    storageBucket: "fpaexpo.firebasestorage.app",
-    messagingSenderId: "769791255019",
-    appId: "1:769791255019:web:38bb03c9d305632797f9ae"
+  apiKey: "AIzaSyAJkuiQEgCgFLd2Uts4b8H6Pwx6izHChkE",
+  authDomain: "fpaexpo.firebaseapp.com",
+  projectId: "fpaexpo",
+  storageBucket: "fpaexpo.firebasestorage.app",
+  messagingSenderId: "769791255019",
+  appId: "1:769791255019:web:38bb03c9d305632797f9ae",
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-// Initialize Auth with AsyncStorage Persistence
 const auth = initializeAuth(app, {
   persistence: getReactNativePersistence(ReactNativeAsyncStorage),
 });
 
-// Initialize Firestore
 const db = getFirestore(app);
 
 export { auth, db };
